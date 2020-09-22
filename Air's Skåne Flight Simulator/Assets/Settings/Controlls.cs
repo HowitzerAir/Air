f2d8929f8bd4f1a0bb7249c1b8b61551e70eba98
HowitzerAir <sebastian.eliason@elev.ga.lbs.se>
2020-09-22 10:41:23 +0200

    Börja på kontroller.
    Jag tänker att vi kan ha settings som är globala. Då blir det enklare att endast ändra script med kontroller istället för dem script som kontroller används i.

--- Air's Skåne Flight Simulator/Assets/Settings/Controlls.cs
+++ Air's Skåne Flight Simulator/Assets/Settings/Controlls.cs
@@ -4,7 +4,28 @@ using UnityEngine;
 
 public class Controlls : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //Kontrollerna kommer att vara globala, så att det går att fixa dem utan att röra movement eller sådant.
+    //Kommentarer kommer jag också att skriva på svenska.
+    //Skall göra så att man själv kan ändra vilka knappar som gör vad.
+
+    public static bool increaseThrottle; //Höja gas
+    public static bool decreaseThrottle; // Sänka gas
+    public static bool bankLeft; //Svänga vänster
+    public static bool bankRight; // Svänga höger
+    public static bool pitchUp; //Höja nosen
+    public static bool pitchDown; //Sänka nosen
+    public static bool yawLeft; //Yaw är axelns som ett rodder styr. Svänga vänster, oftast för att styra på marken
+    public static bool yawRight; //Svänga höger.
+    public static bool flapsUp; //Fäller upp/minksar flaps. Flaps är bitar på vingen som gör att man kan åka snabbare, men som också saktar ner flygplanet.
+    public static bool flapsDown; //Sänker/ökar flaps.
+
+    public enum ThrottleControls { WOchS, PilUpOchNer, ShiftOchCtrl } //Med denna ändrar man vilka knappar som används för gas.
+    public ThrottleControls SelectedMode; //
+
+
+
+
+
     void Start()
     {
         
@@ -13,6 +34,6 @@ public class Controlls : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        /*ThrottleControll[0] = string[3] {"W/S", "ArrowUp/Down", "Shift/Control"}; */
     }
 }
