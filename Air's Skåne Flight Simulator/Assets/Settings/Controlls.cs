f8d1245017d0d4ab63ab452ff68f064bbc1c69d7
HowitzerAir <sebastian.eliason@elev.ga.lbs.se>
2020-09-22 11:33:45 +0200

    Lekte med kontrollerna
    Har jobbat på kontrollera, och kommer att jobba mer på dem senare.

--- Air's Skåne Flight Simulator/Assets/Settings/Controlls.cs
+++ Air's Skåne Flight Simulator/Assets/Settings/Controlls.cs
@@ -8,19 +8,18 @@ public class Controlls : MonoBehaviour
     //Kommentarer kommer jag också att skriva på svenska.
     //Skall göra så att man själv kan ändra vilka knappar som gör vad.
 
-    public static bool increaseThrottle; //Höja gas
-    public static bool decreaseThrottle; // Sänka gas
-    public static bool bankLeft; //Svänga vänster
-    public static bool bankRight; // Svänga höger
-    public static bool pitchUp; //Höja nosen
-    public static bool pitchDown; //Sänka nosen
-    public static bool yawLeft; //Yaw är axelns som ett rodder styr. Svänga vänster, oftast för att styra på marken
-    public static bool yawRight; //Svänga höger.
-    public static bool flapsUp; //Fäller upp/minksar flaps. Flaps är bitar på vingen som gör att man kan åka snabbare, men som också saktar ner flygplanet.
-    public static bool flapsDown; //Sänker/ökar flaps.
+    public static int throttleCommand; //Kontrollera gasen. + är Höja, - är sänka.
+    public static int pitchCommand; //Kontrollera höjdroder.
+    public static int bankCommand; //Kontrollera sväng/svängvinkel, + är höger, - är vänster
+    public static int yawCommand; //Kontrollera planet på marken/från sida till sida.
+    public static int flapsCommand; //Kontrollera klaffarna
+    
 
     public enum ThrottleControls { WOchS, PilUpOchNer, ShiftOchCtrl } //Med denna ändrar man vilka knappar som används för gas.
-    public ThrottleControls SelectedMode; //
+    public ThrottleControls GasKontroll; //
+
+    public enum FlightControlls { wasdFörPitchOchBank = 1, PiltangenterFörPitchOchBank = 2}
+    public FlightControlls KontrollLäge; //Vilka knappar som används för kontroll.
 
 
 
@@ -34,6 +33,43 @@ public class Controlls : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        /*ThrottleControll[0] = string[3] {"W/S", "ArrowUp/Down", "Shift/Control"}; */
+        if (KontrollLäge == FlightControlls.PiltangenterFörPitchOchBank)
+        {
+            KontrollCheck(Input.GetKey(KeyCode.DownArrow), Input.GetKey(KeyCode.UpArrow), pitchCommand);
+            KontrollCheck(Input.GetKey(KeyCode.RightArrow), Input.GetKey(KeyCode.LeftArrow), bankCommand);
+            KontrollCheck(Input.GetKey(KeyCode.Period), Input.GetKey(KeyCode.Comma), yawCommand);
+        }
+        else if(KontrollLäge == FlightControlls.wasdFörPitchOchBank)
+        {
+            KontrollCheck(Input.GetKey(KeyCode.S), Input.GetKey(KeyCode.W), pitchCommand);
+            KontrollCheck(Input.GetKey(KeyCode.D), Input.GetKey(KeyCode.A), bankCommand);
+            KontrollCheck(Input.GetKey(KeyCode.E), Input.GetKey(KeyCode.Q), yawCommand);
+        }
+        if(Input.GetKey(KeyCode.W))
+        {
+            print("Pitch Down");
+        }
+        if(pitchCommand == -1)
+        {
+            print("Pitch Up");
+        }
     }
+
+    #region KontrolMetoder
+    void KontrollCheck(bool CommandUp, bool CommandNer, int commandToChange)
+    {
+        if (CommandUp)
+        {
+            commandToChange = 1;
+        }
+        else if (CommandNer)
+        {
+            commandToChange = -1;
+        }
+        else
+        {
+            commandToChange = 0;
+        }
+    }
+    #endregion
 }
