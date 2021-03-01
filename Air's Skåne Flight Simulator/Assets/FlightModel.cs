0eed78efe6bbb135c0d4e791df99886d63bc7bd6
Squidofpixels <hampus.mollerstromolsson@elev.ga.lbs.se>
2021-03-01 10:40:23 +0100

    Revert "Merge branch 'master' of https://github.com/HowitzerAir/Air"
    This reverts commit a390bb9b4ac37d2d647f87088ab8bbe52ccba471, reversing
    changes made to 11b0435dc90503c59ce2b015279c76e245871982.

--- Air's Skåne Flight Simulator/Assets/FlightModel.cs
+++ Air's Skåne Flight Simulator/Assets/FlightModel.cs
@@ -19,7 +19,6 @@ public class FlightModel : MonoBehaviour
     public float weight;
     public float CL;
     public float surfacedrag;
-    public float bankTorque;
     void Start()
     {
 
@@ -38,30 +37,18 @@ public class FlightModel : MonoBehaviour
 
         if (Controlls.bankCommand > 0)
         {
-            aircraft.AddTorque(0, bankTorque, 0);
+            aircraft.AddTorque(0, 25, 0);
         }
         else if (Controlls.bankCommand < 0)
         {
-            aircraft.AddTorque(0, -bankTorque, 0);
+            aircraft.AddTorque(0, -25, 0);
         }
         else
         {
-            
-            aircraft.AddTorque(0, -aircraft.angularVelocity.y, 0);
+            aircraft.AddTorque(0, 0, 0);
         }
 
-        if(Controlls.pitchCommand > 0)
-        {
-            aircraft.AddTorque(30, 0, 0);
-        }
-        else if(Controlls.pitchCommand < 0)
-        {
-            aircraft.AddTorque(-30, 0, 0);
-        }
-        else
-        {
-            aircraft.AddTorque(-aircraft.angularVelocity.x, 0, 0);
-        }
+
 
     }
 }
