a57c4358986a5e89be79edabc8566c79f3eb3e95
HowitzerAir <71257044+HowitzerAir@users.noreply.github.com>
2021-03-01 10:33:59 +0100

    Revert "Revert "Spelbara ändringar.""
    This reverts commit fcc0acd79fae12608712210d616912fb25be86e0.

--- Air's Skåne Flight Simulator/Assets/FlightModel.cs
+++ Air's Skåne Flight Simulator/Assets/FlightModel.cs
@@ -19,6 +19,7 @@ public class FlightModel : MonoBehaviour
     public float weight;
     public float CL;
     public float surfacedrag;
+    public float bankTorque;
     void Start()
     {
 
@@ -37,18 +38,30 @@ public class FlightModel : MonoBehaviour
 
         if (Controlls.bankCommand > 0)
         {
-            aircraft.AddTorque(0, 25, 0);
+            aircraft.AddTorque(0, bankTorque, 0);
         }
         else if (Controlls.bankCommand < 0)
         {
-            aircraft.AddTorque(0, -25, 0);
+            aircraft.AddTorque(0, -bankTorque, 0);
         }
         else
         {
-            aircraft.AddTorque(0, 0, 0);
+            
+            aircraft.AddTorque(0, -aircraft.angularVelocity.y, 0);
         }
 
-
+        if(Controlls.pitchCommand > 0)
+        {
+            aircraft.AddTorque(30, 0, 0);
+        }
+        else if(Controlls.pitchCommand < 0)
+        {
+            aircraft.AddTorque(-30, 0, 0);
+        }
+        else
+        {
+            aircraft.AddTorque(-aircraft.angularVelocity.x, 0, 0);
+        }
 
     }
 }
