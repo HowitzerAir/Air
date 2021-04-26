b3a530063fc4533a9ab946a6f4aeb5ed7c27a57f
HowitzerAir <71257044+HowitzerAir@users.noreply.github.com>
2021-04-26 10:20:03 +0200

    Scenery

--- Air's Skåne Flight Simulator/Assets/FlightModel.cs
+++ Air's Skåne Flight Simulator/Assets/FlightModel.cs
@@ -19,6 +19,9 @@ public class FlightModel : MonoBehaviour
     public float weight;
     public float CL;
     public float surfacedrag;
+    float AoA;
+    Vector3 aircraftRotation;
+    Vector3 aircraftTracking;
     void Start()
     {
 
@@ -27,13 +30,20 @@ public class FlightModel : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        aircraftRotation = transform.TransformDirection(transform.forward);
+        aircraftTracking = transform.TransformVector(Vector3.forward);
+
+        print("A/C Rotation: " + aircraftRotation);
+        print("A/C Tracking: " + aircraftTracking);
+
+        AoA = Vector3.Angle(aircraftRotation, aircraftTracking);
+        print("AoA: " + AoA);
         speed = aircraft.velocity.magnitude;
         lift = ((airDensity * speed) / 2) * wingarea;
 
         aircraft.AddRelativeForce(0, lift, 0);
-        aircraft.AddRelativeForce(0, 0, engineScript.propThrust);
+        aircraft.AddRelativeForce(0, 0, (engineScript.propThrust*0.3864f));
         print(aircraft.velocity + "m/s");
-
 
         if (Controlls.bankCommand > 0)
         {
@@ -42,6 +52,19 @@ public class FlightModel : MonoBehaviour
         else if (Controlls.bankCommand < 0)
         {
             aircraft.AddTorque(0, -25, 0);
+        }
+        else
+        {
+            aircraft.AddTorque(0, 0, 0);
+        }
+
+        if (Controlls.pitchCommand > 0)
+        {
+            aircraft.AddTorque(0, 0, 25);
+        }
+        else if (Controlls.pitchCommand < 0)
+        {
+            aircraft.AddTorque(0, 0, -25);
         }
         else
         {
